3bd1b92d1c09e1415f929d88bf1c7ea7bd6c67a0
Miles Macklin <miles.macklin@gmail.com>
2013-02-16 09:19:59 +1300

    Working geometry shader billboarding

--- core/shader.h
+++ core/shader.h
@@ -6,10 +6,9 @@
 #include <external/glut/glut.h>
 
 #elif __APPLE__
-
-#include <opengl/opengl.h>
+#define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED 
+#include <opengl/gl3.h>
 #include <glut/glut.h>
-
 #elif PLATFORM_IOS
 
 #if OGL1
@@ -29,7 +28,7 @@
 void glAssert(const char* msg, long line, const char* file);
 
 GLuint CompileProgramFromFile(const char *vertexPath, const char *fragmentPath);
-GLuint CompileProgram(const char *vsource, const char *fsource);
+GLuint CompileProgram(const char *vsource=NULL, const char *fsource=NULL, const char* gsource=NULL);
 GLuint CompileProgram(const char *vsource, const char* csource, const char* esource, const char* fsource);
 
 void DrawPlane(const Vec4& p);
